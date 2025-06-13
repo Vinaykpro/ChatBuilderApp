364454ce790435b1575aa74314bc576ebacdd553
Vinaykpro <89635519+Vinaykpro@users.noreply.github.com>
2025-06-13 13:16:55 +0530

    Added Style screens & chat header component

--- app/src/main/java/com/vinaykpro/chatbuilder/ui/components/SelectModeWidget.kt
+++ app/src/main/java/com/vinaykpro/chatbuilder/ui/components/SelectModeWidget.kt
@@ -1,10 +1,15 @@
 package com.vinaykpro.chatbuilder.ui.components
 
+import android.provider.MediaStore.Images
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview
 @Composable
 fun SelectModeWidget() {
-
 }
